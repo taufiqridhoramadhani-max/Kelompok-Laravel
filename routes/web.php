2d3b65cfd8eccb92e7ed30b4cfc538050708275a
Revalino28 <evallino12345@gmail.com>
2026-06-06 19:47:17 +0700

    Progress Halaman Isi

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\IsiController;
+
+Route::get('/isi', [IsiController::class, 'index']);
