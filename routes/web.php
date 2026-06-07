130726c6210dcae19b9f4af662e7d17a6a9f6698
sonikurniawan830-commits <sonikurniawan830@gmail.com>
2026-06-07 11:21:04 +0700

    Menambah halaman 3 & memperbaiki tampilan keseluruhan

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\IsiController;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/isi', [IsiController::class, 'index']);
+Route::get('/project', [ProjectController::class, 'index']);
